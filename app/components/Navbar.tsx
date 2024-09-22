ffa682892cbda0267eedfef1ed6362999dda9bbb
salmafadlabdulrahman <salmafadlabdulrahman@gmail.com>
2024-09-22 21:51:51 +0300

    add images to the hero

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,16 +4,17 @@ import {
 } from "@heroicons/react/24/solid";
 import Image from "next/image";
 
+
 const Navbar = () => {
   return (
-    <header className="py-4 px-3 bg-white fixed w-full">
-      <nav className="flex items-center justify-between px-3 ">
+    <header className="py-4 px-3 bg-white fixed w-full ">
+      <nav className="flexBetween px-3">
         <div className="flex items-center gap-3">
           <Bars3Icon className="size-6 text-gray-400" />
           <Image src={"/yummly.svg"} alt="logo" width={65} height={65} />
         </div>
-        <div className="flex items-center gap-6 px-3">
-          <Image src={"/cart.svg"} alt="cart" width={30} height={30} />
+        <div className="flex items-center gap-5 pr-[10]">
+          <Image src={"/cart.svg"} alt="cart" width={25} height={25} />
           <MagnifyingGlassIcon className="size-6" />
         </div>
       </nav>
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,35 @@
+import Image from "next/image";
+import "./globals.css";
+
 export default function Home() {
   return (
-      <div>
-        <h1 className="underline text-3xl">Hello World</h1>
-      </div>
+    <main>
+      <section>
+        <div
+          style={{
+            backgroundImage: "url('/banner-marble-bkg.jpg')",
+            backgroundPosition: "center",
+            backgroundSize: "cover",
+            height: "100%",
+            width: "100%",
+          }}
+          className="flex items-center justify-between"
+        >
+          <Image
+            alt="strawberries bowl"
+            src={"/strawberry-grain.webp"}
+            width={170}
+            height={100}
+            className=""
+          />
+          <Image
+            alt="blue berries bowl"
+            src={"/img-fruit-bowl.webp"}
+            width={210}
+            height={200}
+          />
+        </div>
+      </section>
+    </main>
   );
 }
