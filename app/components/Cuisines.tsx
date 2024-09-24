beec990abc41d347185578352ca75875800d1edb
salmafadlabdulrahman <salmafadlabdulrahman@gmail.com>
2024-09-24 22:48:57 +0300

    update cuisines section styling

--- app/components/Cuisines.tsx
+++ app/components/Cuisines.tsx
@@ -8,14 +8,21 @@ const Cuisines = ({ img, title }: CuisineProps) => {
     <div className="">
       <div className="text-center w-[130px]">
         {" "}
-        <Image
-          src={`/cuisines/${img}`}
-          alt="cuisine image"
-          height={200}
-          width={200}
-          className="object-cover rounded-full aspect-square"
-        />
-        <span>{title}</span>
+        <div className="relative">
+          <Image
+            src={`/cuisines/${img}`}
+            alt="cuisine image"
+            height={200}
+            width={200}
+            className="object-cover rounded-full aspect-square"
+          />
+          <div className="overlay bg-[#000000] bg-opacity-35 absolute bottom-0 w-[130px] h-[130px] object-cover rounded-full aspect-square"></div>
+          <div className="flex justify-center">
+            <span className="absolute text-center top-[57px] text-white uppercase text-[12px] font-semibold tracking-wider">
+              {title}
+            </span>
+          </div>
+        </div>
       </div>
     </div>
   );
