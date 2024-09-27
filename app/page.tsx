2f126b91dc89bf38ad4c484d0b5e82ac876a3c18
salmafadlabdulrahman <salmafadlabdulrahman@gmail.com>
2024-09-27 22:49:18 +0300

    update the recommendations

--- app/page.tsx
+++ app/page.tsx
@@ -60,7 +60,7 @@ export default function Home() {
             width={145}
           />
         </div>
-        <p className="mt-5 text-[#bababa] text-[13px] font-semibold text-center flex justify-end w-[84%]">
+        <p className="mt-5 text-[#bababa] text-[13px] font-semibold text-center lg:flex lg:justify-end lg:w-[84%]">
           Don&apos;t Personalize My Recommendations
         </p>
       </div>
